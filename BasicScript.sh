77221e26c6a990cf44a7f44df035b577502b35a3
sabdulit <sabdulit@gmail.com>
2023-10-07 13:55:13 +0530

    Updated

--- BasicScript.sh
+++ BasicScript.sh
@@ -1,3 +1,14 @@
 #/bin/bash
 echo "HelloWorld"
+echo "Enter Your Name:"
+read name
+echo "Hi ,$name! How Are Today"
+echo "Enter a number between 1-10:"
+read counter
+while [ $counter -ge 0 ]
+do
+echo $counter
+((counter--))
+done
+echo TADA
 
